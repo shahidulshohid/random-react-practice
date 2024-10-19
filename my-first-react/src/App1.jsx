12f7893446693b532d745f1f3529c7c9d90e16ef
shahidul shohid <shahidulislamshohid7gmail.com>
2024-10-19 20:01:47 +0600

    practice is done

---
+++ my-first-react/src/App1.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Square from './components1/Square';
+
+function App1() {
+  return (
+    <div>
+        <h1>shahdiul islam shohid</h1>
+        <Square></Square>
+    </div>
+  );
+}
+
+export default App1
